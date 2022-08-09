08ee4c10110cbf15471a6be829caebb2c136790a
Naman Malhan <namanmalhan18@gmail.com>
2022-08-09 10:47:28 +0530

    Intial commit

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Navigation.module.css'
+
+const Navigation = () => {
+    return (
+        <nav>
+            <img src="images/logo10.png" alt="logo" className={styles.logo} />
+        </nav>
+    )
+}
+
+export default Navigation
